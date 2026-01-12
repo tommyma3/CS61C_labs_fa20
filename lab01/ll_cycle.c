7d1eca77699b2a183d8701111079652a81f08894
Tommy Ma <124090467@link.cuhk.edu.cn>
2026-01-12 13:15:53 +0800

    done with lab01

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,24 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    node *tortoise = head;
+    node *hare = head;
+
+    while (1) {
+        for (int i = 0; i < 2; ++i) {
+            if (hare == NULL) {
+                return 0;
+            } else {
+                hare = hare->next;
+            }
+        }
+
+        tortoise = tortoise->next;
+
+        if (tortoise == hare) {
+            return 1;
+        }
+    }
+
     return 0;
 }
