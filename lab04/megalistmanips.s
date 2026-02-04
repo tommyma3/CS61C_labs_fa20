644c755895cdbdfdac830c9ab36ddc0cc77e801c
Tommy Ma <124090467@link.cuhk.edu.cn>
2026-02-04 14:48:12 +0800

    done with lab04

--- lab04/megalistmanips.s
+++ lab04/megalistmanips.s
@@ -46,10 +46,12 @@ main:
     ecall
 
 map:
-    addi sp, sp, -12
+    addi sp, sp, -20
     sw ra, 0(sp)
     sw s1, 4(sp)
     sw s0, 8(sp)
+    sw a1, 12(sp)
+    sw a0, 16(sp)
 
     beq a0, x0, done    # if we were given a null pointer, we're done.
 
@@ -66,27 +68,41 @@ map:
     # are modified by the callees, even when we know the content inside the functions 
     # we call. this is to enforce the abstraction barrier of calling convention.
 mapLoop:
-    add t1, s0, x0      # load the address of the array of current node into t1
+    lw t1, 0(s0)      # load the address of the array of current node into t1
     lw t2, 4(s0)        # load the size of the node's array into t2
 
+    slli t0, t0, 2
     add t1, t1, t0      # offset the array address by the count
+    srai t0, t0, 2
     lw a0, 0(t1)        # load the value at that address into a0
 
+    addi sp, sp, -12
+    sw t2, 0(sp)
+    sw t1, 4(sp)
+    sw t0, 8(sp)
+
     jalr s1             # call the function on that value.
+
+    lw t0, 8(sp)
+    lw t1, 4(sp)
+    lw t2, 0(sp)
+    addi sp, sp, 12
 
     sw a0, 0(t1)        # store the returned value back into the array
     addi t0, t0, 1      # increment the count
     bne t0, t2, mapLoop # repeat if we haven't reached the array size yet
 
-    la a0, 8(s0)        # load the address of the next node into a0
-    lw a1, 0(s1)        # put the address of the function back into a1 to prepare for the recursion
+    lw a0, 8(s0)        # load the address of the next node into a0
+    add a1, s1, x0        # put the address of the function back into a1 to prepare for the recursion
 
     jal  map            # recurse
 done:
+    lw a0, 16(sp)
+    lw a1, 12(sp)
     lw s0, 8(sp)
     lw s1, 4(sp)
     lw ra, 0(sp)
-    addi sp, sp, 12
+    addi sp, sp, 20
     jr ra
 
 mystery:
